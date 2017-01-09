bae13f6ec26994ce8a3b7aa1258ac731fd2ad326
billyblondking <zhuhongwei1339@foxmail.com>
2017-01-09 13:56:25 +0800

    shoppingcart: 界面

--- app/src/main/java/demo/groupnine/taobaodemo/shoppingcart/CartFragment.java
+++ app/src/main/java/demo/groupnine/taobaodemo/shoppingcart/CartFragment.java
@@ -3,13 +3,42 @@ package demo.groupnine.taobaodemo.shoppingcart;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.*;
+
+import java.util.List;
+
 import demo.groupnine.taobaodemo.R;
 
 public class CartFragment
         extends Fragment {
+    private static String TAG = "CartFragment";
+
+    // data model
+    private List<ShoppingCart> mCart;
+    //result widget
+    private RecyclerView mCartRecyclerView;
+    private CartAdapter mAdapter;
+    private View mBlankListPrompt;
 
     // lifetime methods
+
+    public static CartFragment newInstance(String keyword)
+    {
+        CartFragment fragment = new CartFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(
+            @Nullable
+                    Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+        fetchResult();
+        getActivity().setTitle("Cart");
+    }
 
     @Nullable
     @Override
