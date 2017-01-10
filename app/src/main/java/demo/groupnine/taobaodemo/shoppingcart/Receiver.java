aef224abad98dbecf6d07f94441b6b91120a5c80
Robert Darwin <robert.darwin@outlook.com>
2017-01-10 14:38:51 +0800

    confirmorder: 添加 Receiver 类

---
+++ app/src/main/java/demo/groupnine/taobaodemo/shoppingcart/Receiver.java
@@ -0,0 +1,11 @@
+package demo.groupnine.taobaodemo.shoppingcart;
+
+
+public class Receiver {
+    public String userId;
+    public String receiverId;
+    public String receiverName;
+    public String receiverAddress;
+    public String receiverPhone;
+    public String usedTimes;
+}
